48b8b50cbdae90dd28667c6937892ef5478484b1
dimasabimanyu <dmsabmny@gmail.com>
2023-02-02 01:45:08 +0700

    updated the content of section and works

---
+++ js/document.js
@@ -0,0 +1,42 @@
+// Text content from this site
+
+const aboutMe = `About Me
+
+I am a highly motivated software engineer with over 6 years of professional experience designing and developing sophisticated applications. My expertise lies in areas such as software architecture, mobile application development, and UX/UI design.
+
+
+In addition to my technical qualifications, I am a team-oriented professional with strong communication and problem-solving skills. I have worked on many successful projects in a wide variety of industries and am confident in my ability to quickly adapt to unfamiliar situations and technologies.
+
+
+My ultimate goal is to help companies and individuals build software that can make a real impact on the world. I believe that my passion for software engineering, combined with my technical knowledge, will help me to achieve this.`;
+
+
+const aboutMeTwo = `"I am a highly motivated and enthusiastic software engineer with a specialization in frontend development. With a passion for creating visually appealing and user-friendly web experiences. I currently working with Node and JavaScript ecosystems. However, I am always eager to expand my knowledge and learn new languages and technologies. My ultimate goal is to deliver innovative and high-quality solutions that make a meaningful impact. With strong problem-solving skills and a drive to continuously improve, I am confident in my ability to meet any challenge and deliver outstanding results."`;
+
+// Here are a few sentences you could use for your hero section on your software engineer portfolio:
+
+// "Building beautiful and functional web experiences with a passion for code."
+// "Crafting elegant solutions to complex problems with a focus on user experience."
+// "Bringing ideas to life with a relentless drive for innovation and excellence."
+// "Empowering businesses with cutting-edge technology and a commitment to quality."
+// "Transforming the digital landscape with a focus on simplicity, performance, and scalability."
+// "Elevating user experiences through creative and intuitive design, powered by code."
+
+// Video Explanation on Section Projects
+const githubProfile = `
+GitHub Profile is a project that aims to provide a more visually appealing and informative view of a user's GitHub profile and repository statistics. It uses data visualization to display the user's top languages and the number of stars and forks on their repositories. The project allows users to sort through their top repositories based on the number of stars and forks they have received. The goal of the project is to make it easier for users to understand and navigate their GitHub data.
+
+GitHub Profile provides data visualization of the user's top programming languages and displays the number of stars and forks for each repository. The project also includes the ability to sort through the user's top repositories by the number of stars and forks they have received. In short, this project is a tool to showcase your GitHub profile and repository statistics in a more visually appealing and interactive way.
+`
+
+// hotel reservation
+// A hotel reservation side project using React JS and Contentful headless CMS. Features include dynamic room booking, visually appealing interface, fast load times, and easy content management via CMS. Built for user-friendly hotel booking experience.
+
+// covid tracker
+// A COVID-19 information website built with data from disease.sh API and charts powered by chart.js. Provides real-time data on global cases, trends, and analysis in a visually compelling format.
+
+// seafoam theme
+// a dark green theme for Visual Studio Code to enhance the coding experience with a stylish, relaxing and eye-friendly color scheme.
+
+// gihub profile
+// A GitHub profiler website using Fusion Charts to display repository statistics, including top languages and stars, sorted by stars and forks. Provides a comprehensive analysis of your GitHub activity with engaging data visualizations.
